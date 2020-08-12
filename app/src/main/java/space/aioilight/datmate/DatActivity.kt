511927359e9003f91ac7a8f2ebfd6be5e83224a1
AioiLight <info@aioilight.space>
2020-08-12 13:41:28 +0900

    DatAcitivity

---
+++ app/src/main/java/space/aioilight/datmate/DatActivity.kt
@@ -0,0 +1,11 @@
+package space.aioilight.datmate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dat)
+    }
+}
